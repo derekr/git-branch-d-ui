f977a8b9f57ef942d71a497b77da8757918665bb
Derek <derekr@me.com>
2013-12-18 21:33:12 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+var fs = require('fs');
+var formidable = require('formidable');
+var git = require('./lib/git')(__dirname);
+var http = require('http');
+var hyperglue = require('hyperglue');
+var _ = require('lodash');
+var html = fs.readFileSync(__dirname + '/index.html');
+var port = process.env.PORT || 8080;
+
+function handlePost (req, res) {
+    var form = new formidable.IncomingForm();
+
+    form.parse(req, function (err, fields) {
+        console.dir(fields);
+        res.writeHead(200);
+        res.end('git branch -D ' + fields.branches.join(' '));
+    });
+}
+
+function handleGet (req, res) {
+    git.branches(function (err, branches) {
+        var data = _.map(branches, function (b) {
+            return {
+                '.branch-id': { value: b },
+                '.branch-name': b
+            }
+        });
+
+        res.writeHead(200);
+        res.end(hyperglue(html, {
+            '.branch-item': data
+        }).outerHTML);
+    });
+}
+
+var server = http.createServer(function (req, res) {
+    if (req.method === 'POST') return handlePost(req, res);
+
+    handleGet(req, res);
+});
+
+server.listen(port);
+console.log('Listening on ' + port);
---
+++ readme.md
@@ -0,0 +1,26 @@
+# git branch d ui
+#### Web ui for deleting git branches
+
+Sometimes I cut a lot of branches. **A LOT** of branches. They grow stale and 
+I need to clear them out. I am probably dumb and missing something in my normal 
+git workflow, but I thought something like this would be handy.
+
+## Usage
+
+```
+cd /path/to/repo
+git-branch-d-ui
+```
+
+This starts a server at [http://localhost:8080]() that will render a list 
+of all branches for the repo. You can then check the repos you want to delete 
+and click the "Delete" button. This will return a text response of:
+
+```
+git branch -D [selected branches]
+```
+
+## Future
+
+Currently this is the most basic implementation to get the job done, but I think 
+it would be cool to mimic github's branch list w/ branch stats and what not. 
